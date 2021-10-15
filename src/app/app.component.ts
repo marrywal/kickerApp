b260f9422213dfc01a6e27fff8915ef1f31d1fb1
Maria Walzl <maria.walzl@mayflower.de>
2021-10-15 09:23:26 +0200

    naming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'kickerApp';
   open = false;
 
   constructor(public router: Router){}
